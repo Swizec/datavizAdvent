00f11fdcab01aa2f24f820155d52ed94defc3da0
CraftAtoms <creator@craftatoms.com>
2018-12-08 08:11:40 -0500

    Helmet10

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,25 +35,10 @@ module.exports = {
             path: `${__dirname}/src/content`
             }
         },
-        {
-            resolve: `gatsby-transformer-remark`,
-            options: {
-                plugins: [
-                    {
-                        resolve: `gatsby-remark-responsive-iframe`,
-                        options: {
-                            wrapperStyle: `margin-bottom: 1.0725rem`,
-                        },
-                    },
-                    
-                    'gatsby-remark-prismjs',
-                    
-                    'gatsby-remark-copy-linked-files',
-                    'gatsby-remark-smartypants',
-                ],
-            },
-        },
         
+        'gatsby-remark-prismjs',
+        'gatsby-remark-copy-linked-files',
+        'gatsby-remark-smartypants',
         `gatsby-transformer-sharp`,
         
 
